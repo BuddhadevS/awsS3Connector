8aa813faa8ef464f454711c5e1188e9963d0b9dc
buddhadevS <buddhadev0509@gmail.com>
2025-12-19 00:34:48 +0530

    Added a extra file

---
+++ src/main/java/com/tek/aws/controller/AwsTestConnector.java
@@ -0,0 +1,10 @@
+package com.tek.aws.controller;
+
+public class AwsTestConnector {
+
+    @Override
+    public String toString() {
+        System.out.println("Aws Test Connector");
+        return "AwsTestConnector";
+    }
+}
